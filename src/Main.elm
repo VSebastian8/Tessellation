3a3ee03775a7c5839bc2d065a25d6ad75ffcaa56
Visan Sebastian <emanuel-sebastian.visan-draghicescu@s.unibuc.ro>
2025-04-05 00:24:30 +0300

    Polygon Drawing

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,17 @@
 module Main exposing (main)
 
+import ColorTheme exposing (Color(..))
 import Html exposing (Html, div)
 import Html.Attributes exposing (style)
+import Polygon exposing (..)
 import Regular exposing (..)
 import Svg exposing (svg)
 import Svg.Attributes exposing (height, viewBox, width)
+
+
+equilateral : Polygon
+equilateral =
+    { lengths = [ 10, 10, 10, 20, 20 ], angles = [ 120, 120, 120, 120, 60 ], rotation = 60 }
 
 
 main : Html msg
@@ -17,9 +24,9 @@ main =
         , style "margin" "0"
         ]
         [ svg
-            [ viewBox "100 100 900 900"
+            [ viewBox "0 0 800 800"
             , width "800"
             , height "800"
             ]
-            (hexagons 100 100)
+            [ polygonSvg equilateral 10 Secondary { x = 200, y = 200 } ]
         ]
---
+++ src/Polygon.elm
@@ -0,0 +1,74 @@
+module Polygon exposing (..)
+
+import ColorTheme exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Util exposing (..)
+
+
+type alias Polygon =
+    { lengths : List Float
+    , angles : List Float
+    , rotation : Float
+    }
+
+
+startAt : Polygon -> Int -> Polygon
+startAt { lengths, angles, rotation } n =
+    { lengths = List.drop n lengths ++ List.take n lengths
+    , angles = List.drop n angles ++ List.take n angles
+    , rotation = rotation
+    }
+
+
+rotate : Polygon -> Float -> Polygon
+rotate { lengths, angles } a =
+    { lengths = lengths
+    , angles = angles
+    , rotation = a
+    }
+
+
+polygonPoints : Polygon -> Float -> List Point
+polygonPoints { lengths, angles, rotation } size =
+    List.map2 Tuple.pair (List.map (\l -> l * size) lengths) (rotation + 180 :: angles)
+        |> pointSequence { x = 0, y = 0 } 0
+
+
+pointSequence : Point -> Float -> List ( Float, Float ) -> List Point
+pointSequence point rotation polyList =
+    case polyList of
+        [] ->
+            []
+
+        ( length, angle ) :: rest ->
+            let
+                next_rotation =
+                    180 + rotation + angle
+
+                next_point =
+                    { x = point.x + length * cos (degrees next_rotation), y = point.y - length * sin (degrees next_rotation) }
+            in
+            point :: pointSequence next_point next_rotation rest
+
+
+startPoint : List Point -> Point -> List Point
+startPoint points p =
+    points |> List.map (\q -> { x = q.x + p.x, y = q.y + p.y })
+
+
+polygonSvg : Polygon -> Float -> Color -> Point -> Svg msg
+polygonSvg poly size color point =
+    let
+        svgPoints =
+            startPoint (polygonPoints poly size) point
+                |> List.map (\p -> String.fromFloat p.x ++ "," ++ String.fromFloat p.y)
+                |> String.join " "
+    in
+    polygon
+        [ points svgPoints
+        , fill (theme.getColor color)
+        , stroke theme.strokeColor
+        , strokeWidth "2"
+        ]
+        []
